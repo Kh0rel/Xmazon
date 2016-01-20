d6244e9428e7e66278a0a71df7aca79691630462
shemana <guillaumeChb@MacBook-Air-de-guillaume-2.local>
2016-01-20 10:27:52 +0100

    Initial Commit

---
+++ Xmazon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xmazon
+//
+//  Created by guillaume chieb bouares on 20/01/2016.
+//  Copyright (c) 2016 com.esgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Xmazon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Xmazon
+//
+//  Created by guillaume chieb bouares on 20/01/2016.
+//  Copyright (c) 2016 com.esgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
