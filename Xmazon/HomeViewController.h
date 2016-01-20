9276896bb4d174505456c841f6e7c4950c33db3d
shemana <guillaumeChb@MacBook-Air-de-guillaume-2.local>
2016-01-20 20:54:07 +0100

    OAuth2Client framework added, Main.storyboard deleted HomeViewController.h,m,xib added

--- Xmazon/HomeViewController.h
+++ Xmazon/HomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HomeViewController.h
 //  Xmazon
 //
 //  Created by guillaume chieb bouares on 20/01/2016.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HomeViewController : UIViewController
 
 @end
-
