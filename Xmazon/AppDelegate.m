2a1bea25756dfbe19c7a3d018a9791c05a6d2d80
shemana <guillaume.chiebbouares@gmail.com>
2016-02-03 08:12:08 +0100

    LoginView xib modified

--- Xmazon/AppDelegate.m
+++ Xmazon/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "HomeViewController.h"
+#import "LoginViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     UIWindow* window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[HomeViewController new]];
+    window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[LoginViewController new]];
     [window makeKeyAndVisible];
     self.window = window;
     return YES;
---
+++ Xmazon/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Xmazon
+//
+//  Created by guillaume chieb bouares on 01/02/2016.
+//  Copyright © 2016 com.esgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
