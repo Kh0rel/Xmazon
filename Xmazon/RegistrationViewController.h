e6cf4db739c42174af150eb7483fb306f78b86fe
shemana <guillaume.chiebbouares@gmail.com>
2016-02-03 08:23:00 +0100

    Registration class added

---
+++ Xmazon/RegistrationViewController.h
@@ -0,0 +1,15 @@
+//
+//  RegistrationViewController.h
+//  Xmazon
+//
+//  Created by guillaume chieb bouares on 03/02/2016.
+//  Copyright © 2016 com.esgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegistrationViewController : UIViewController{
+    
+}
+
+@end
--- Xmazon/RegistrationViewController.m
+++ Xmazon/RegistrationViewController.m
@@ -1,31 +1,28 @@
 //
-//  PasswordNotFoundViewController.m
+//  RegistrationViewController.m
 //  Xmazon
 //
 //  Created by guillaume chieb bouares on 03/02/2016.
 //  Copyright © 2016 com.esgi. All rights reserved.
 //
 
-#import "PasswordNotFoundViewController.h"
+#import "RegistrationViewController.h"
 
-@interface PasswordNotFoundViewController ()
+@interface RegistrationViewController ()
 
 @end
 
-@implementation PasswordNotFoundViewController
+@implementation RegistrationViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"Mot de passe oublié";
+    self.title = @"Inscription";
     // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-- (IBAction)resetButtonAction:(id)sender {
-    
 }
 
 /*
