6054fc3fc78eb6f1353ee5b0c28c773865b3b17d
shemana <guillaume.chiebbouares@gmail.com>
2016-03-01 00:05:22 +0100

    stabilize merge with master

--- Xmazon/RearViewController.m
+++ Xmazon/RearViewController.m
@@ -42,7 +42,7 @@
     [self.view insertSubview:treeView atIndex:0];
     
     [self.treeView registerNib:[UINib nibWithNibName:NSStringFromClass([RATableViewCell class]) bundle:nil] forCellReuseIdentifier:NSStringFromClass([RATableViewCell class])];
-
+    
 }
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
@@ -127,57 +127,57 @@
     return data.children[index];
 }
 /*
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 4;
-}
--(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSString* cellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    NSInteger row = indexPath.row;
-    
-    if (nil == cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];
-    }
-    
-    NSString* text = nil;
-    if (row == 0) {
-        text = @"Accueil";
-    }
-    else if (row == 1){
-        text = @"Profil";
-    }
-    else if (row == 2){
-        text = @"Catégorie";
-    }
-    else if (row == 3){
-        text = @"Historique des commandes";
-    }
-    cell.textLabel.text = (![text isEqual:nil]) ? text : nil;
-    
-    return cell;
-}
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    SWRevealViewController* revealController = self.revealViewController;
-    UIViewController* newFrontView = nil;
-    
-    
-    if(indexPath.row == 0){
-        newFrontView = [HomeViewController new];
-    }
-    else if (indexPath.row == 1){
-        newFrontView = [ProfileViewController new];
-    }
-    else if (indexPath.row == 2){
-        newFrontView = [CategoryViewController new];
-    }
-    else if (indexPath.row == 3){
-        newFrontView = [OrderHistoryViewController new];
-    }
-
-    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:newFrontView];
-    [revealController pushFrontViewController:navigationController animated:YES];
-}
-*/
+ - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+ return 4;
+ }
+ -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+ static NSString* cellIdentifier = @"Cell";
+ UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+ NSInteger row = indexPath.row;
+ 
+ if (nil == cell) {
+ cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];
+ }
+ 
+ NSString* text = nil;
+ if (row == 0) {
+ text = @"Accueil";
+ }
+ else if (row == 1){
+ text = @"Profil";
+ }
+ else if (row == 2){
+ text = @"Catégorie";
+ }
+ else if (row == 3){
+ text = @"Historique des commandes";
+ }
+ cell.textLabel.text = (![text isEqual:nil]) ? text : nil;
+ 
+ return cell;
+ }
+ -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+ SWRevealViewController* revealController = self.revealViewController;
+ UIViewController* newFrontView = nil;
+ 
+ 
+ if(indexPath.row == 0){
+ newFrontView = [HomeViewController new];
+ }
+ else if (indexPath.row == 1){
+ newFrontView = [ProfileViewController new];
+ }
+ else if (indexPath.row == 2){
+ newFrontView = [CategoryViewController new];
+ }
+ else if (indexPath.row == 3){
+ newFrontView = [OrderHistoryViewController new];
+ }
+ 
+ UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:newFrontView];
+ [revealController pushFrontViewController:navigationController animated:YES];
+ }
+ */
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
