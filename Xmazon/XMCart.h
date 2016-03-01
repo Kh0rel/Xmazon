e000440f85e907bdbafc7ce32f6997f389d937c6
shemana <guillaume.chiebbouares@gmail.com>
2016-03-01 22:31:57 +0100

    fixed error

---
+++ Xmazon/XMCart.h
@@ -0,0 +1,13 @@
+//
+//  XMCart.h
+//  Xmazon
+//
+//  Created by guillaume chieb bouares on 01/03/2016.
+//  Copyright © 2016 com.esgi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMCart : NSObject
+
+@end
